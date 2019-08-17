417acd5fb0c46e4ed5686402a636f5dee56e590d
Julian Viechter <julian@veranstaltungstechnik-viechter.de>
2019-08-17 13:58:09 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+window.addEventListener("load", function load() {
+  window.removeEventListener("load", load);
+
+  import("../wasm_build").then(({ BoidPool }) => {
+    const boidPool = BoidPool.new();
+    boidPool.add_boid(100, 100);
+    boidPool.add_boid(200, 200);
+
+    window.addEventListener("beforeunload", () => {
+      boidPool.free();
+    });
+
+    const renderLoop = () => {
+      boidPool.update();
+      boidPool.render();
+      requestAnimationFrame(renderLoop);
+    };
+
+    requestAnimationFrame(renderLoop);
+  });
+});
