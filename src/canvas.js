f72d73f7698ce714836ad63396eced60e4947556
Julian Viechter <julian@veranstaltungstechnik-viechter.de>
2019-08-18 12:19:16 +0200

    Re prettify

--- src/canvas.js
+++ src/canvas.js
@@ -1,27 +1,26 @@
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
-canvas.width = Number(canvas.style.width.replace("px", "")) * 2;
-canvas.height = Number(canvas.style.height.replace("px", "")) * 2;
+canvas.width = Number(canvas.style.width.replace('px', '')) * 2;
+canvas.height = Number(canvas.style.height.replace('px', '')) * 2;
 context.scale(2, 2);
 
 export function clear_canvas() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 export function draw_boid(x, y, angle) {
-  context.save();
-  context.translate(x, y);
-  context.rotate(angle);
+    context.save();
+    context.translate(x, y);
+    context.rotate(angle);
 
-  context.beginPath();
+    context.beginPath();
 
-  [[10, 0], [-10, -6], [-10, 6]].forEach(([x, y], index) =>
-    index === 0 ? context.moveTo(x, y) : context.lineTo(x, y)
-  );
+    [[10, 0], [-10, -6], [-10, 6]].forEach(([x, y], index) =>
+        index === 0 ? context.moveTo(x, y) : context.lineTo(x, y)
+    );
 
-  context.closePath();
-  context.stroke();
-  //context.fill();
-  context.restore();
+    context.closePath();
+    context.stroke();
+    context.restore();
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
-window.addEventListener("load", function load() {
-  window.removeEventListener("load", load);
+window.addEventListener('load', function load() {
+    window.removeEventListener('load', load);
 
-  import("../wasm_build").then(({ BoidPool }) => {
-    const boidPool = BoidPool.new(800, 600);
-    boidPool.add_boid(100, 100);
-    boidPool.add_boid(250, 250);
+    import('../wasm_build').then(({ BoidPool }) => {
+        const boidPool = BoidPool.new(800, 600);
+        boidPool.add_boid(100, 100);
+        boidPool.add_boid(250, 250);
 
-    window.addEventListener("beforeunload", () => {
-      boidPool.free();
+        window.addEventListener('beforeunload', () => {
+            boidPool.free();
+        });
+
+        const renderLoop = () => {
+            boidPool.update();
+            boidPool.render();
+            requestAnimationFrame(renderLoop);
+        };
+
+        requestAnimationFrame(renderLoop);
     });
-
-    const renderLoop = () => {
-      boidPool.update();
-      boidPool.render();
-      requestAnimationFrame(renderLoop);
-    };
-
-    requestAnimationFrame(renderLoop);
-  });
 });
